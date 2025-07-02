c1aa0a18b5867671d7841f2016ace1306f18774f
jubindas <jubinrohidas78@gmail.com>
2025-07-02 10:34:28 +0530

    first init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import TopBar from "./components/topBar/TopBar";
+import Row from "./components/rowBar/Row";
+
+
+export default function App() {
+  return (
+    <div> <TopBar />
+    <Row></Row></div>
+  )
+}
